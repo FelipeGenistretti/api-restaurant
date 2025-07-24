674e91688d49b6bbe1c7733e77d051e7289d6c7c
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2025-07-24 20:45:08 -0300

    Construindo migrations de products e implementando funções no controller

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -1,13 +1,39 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from 'zod';
+import { knex } from "@/database/knex";
 
 class ProductController {
 
     async index (req:Request, res:Response, next:NextFunction){
         try {
-            return res.json({message:"ok"})
+            const { name } = req.query
+            const products = await knex<ProductRepository>("products")
+            .select()
+            .whereLike("name", `%${name ?? ""}%`)
+            .orderBy("name");
+            
+
+            return res.json(products);
         } catch (error) {
             next(error)
             
+        }
+    }
+
+    async create(req:Request, res:Response, next:NextFunction){
+        try {
+            const bodySchema = z.object({
+                name: z.string({ required_error: "name is required" }).trim().min(6),
+                price : z.number({ required_error: "price is required!" }).gt(0, {message:"value must be greater than 0"})
+            })
+
+            const { name, price } = bodySchema.parse(req.body)
+
+            await knex<ProductRepository>("products").insert({ name, price })
+
+            return res.status(201).json()
+        } catch (error) {
+            next(error)
         }
     }
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { log } from "console";
 import express, { Request, Response } from "express";
 import { routes } from "./routes";
+import { errorHandling } from "./middlewares/error-handling";
 
 
 const PORT = 3333;
@@ -8,12 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use(errorHandling);
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);  
+})
 
 app.get("/", async (req:Request, res:Response)=>{
     console.log("teste get");   
 })
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on port ${PORT}`);
-    
-})
